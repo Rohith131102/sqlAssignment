f7955b67e1c8d3fdc7fb3ff7ef727a5b1595591b
Rohith131102 <123619674+Rohith131102@users.noreply.github.com>
2023-02-13 01:09:45 +0530

    Update 1.getNumMaleFemale.sql

--- 1.getNumMaleFemale.sql
+++ 1.getNumMaleFemale.sql
@@ -4,48 +4,46 @@ USE `sqlAssignment`;           -- Using the created schema
 -- Question - 1
 -- Creation of Table 
 
-create table employees (
-  EmpID integer(4) not null unique, 
-  -- As employee id cannot be null and need to be unique
-  EmpName varchar(30), 
-  Gender varchar(10), 
-  Department varchar(30), 
+CREATE TABLE employees (
+  EmpID integer(4) NOT NULL UNIQUE,  -- As employee id needs to be unique and not null
+  EmpName VARCHAR(30), 
+  Gender VARCHAR(10), 
+  Department VARCHAR(30), 
   -- Check constraint for preventing invalid enetries
-  check(
-    Gender in ("Male", "Female")
+  CHECK(
+    Gender IN ("Male", "Female")
   )
 );
 -- Insertion of given data
-Insert into employees 
+INSERT INTO employees 
 values 
   (1, 'X', 'Female', 'Finance'), 
   (2, 'Y', 'Male', 'IT'), 
   (3, 'Z', 'Male', 'HR'), 
   (4, 'W', 'Female', 'IT');
 
--- Data insertion to highlight the Not Assigned row
+-- Insering a example where department entry is NULL 
 Insert into employees 
 values 
   (5, 'V', 'Male', null);
  
 -- Procedure holding Query to find the number of male and female employees in each department
 DELIMITER &&  
-CREATE PROCEDURE getNumMaleFemale()
+CREATE PROCEDURE getNumMaleFemale()     -- creating a procedure 
 BEGIN  
 SELECT 
-  IFNULL(Department, 'Not Assigned') as Department, 
+  IFNULL(Department, 'Not Assigned') as Department, --if Department entry in employees table is null,then we add gender details in Not Assigned
   COUNT(
-    CASE WHEN UPPER(Gender)= 'MALE' THEN 1 END
+    CASE WHEN Gender= 'Male' THEN 1 END        --incrementing the count by 1 whenever male entry is found
   ) AS 'Num of Male', 
   COUNT(
-    CASE WHEN UPPER(Gender)= 'FEMALE' THEN 1 END
+    CASE WHEN Gender= 'Female' THEN 1 END      --incrementing the count by 1 whenever female entry is found
   ) AS 'Num of Female' 
-FROM 
-  employees 
-GROUP BY 
-  Department 
-order by 
-  Department;
+FROM employees 
+GROUP BY Department 
+order by Department;
 END &&  
 DELIMITER ;    
+
+-- calling procedure to get number of male and female detains department wise
 CALL getNumMaleFemale();
